445ed1383797ef58deab20d839eb547388e7c077
daiwk <daiwk1989@gmail.com>
2025-07-14 17:38:38 +0800

    aider

---
+++ llm_apps/run_aider.sh
@@ -0,0 +1,5 @@
+
+export OPENROUTER_API_KEY="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"
+
+aider --list-models openrouter/
+aider --model openrouter/deepseek/deepseek-r1:free
